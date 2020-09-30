40c815fd185dfc6b8e7916905a8e76de4292406f
Joseph Miceli <joe.miceli8@gmail.com>
2020-09-30 16:30:25 -0600

    Update SamplePoints.cpp

--- SamplePoints.cpp
+++ SamplePoints.cpp
@@ -17,7 +17,7 @@ void SamplePoints(std::vector<double>& current_pos, std::vector<std::vector<doub
 		//double theta = i * M_PI/180;
 		double theta = i * 3.1415/180;
 
-
+		//TODO: make the radius of these points equal to the step size, not 1
 
 		sampled_pt[0] = current_pos[0] + cos(theta);
 		sampled_pt[1] = current_pos[1] + sin(theta);
